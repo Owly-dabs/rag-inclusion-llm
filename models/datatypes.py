6edd382be39c1349fad6c7840fd589d4ca283647
Owly-dabs <owlycodes@gmail.com>
2025-06-18 12:57:49 -0700

    add PromptResponse datatype

--- models/datatypes.py
+++ models/datatypes.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from typing import List
 
 
@@ -19,3 +19,16 @@ class PromptRow:
     issue_no: int
     summary: str
     bertopic: int
+
+@dataclass
+class PromptResponse:
+    repo: str
+    issue_no: int
+    topic: str
+    code_regions: List[CodeRegion]
+    
+    def __getitem__(self, key):
+        return asdict(self)[key]
+
+    def __iter__(self):
+        return iter(asdict(self))
